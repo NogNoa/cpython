cb46ae32603177a11074ca2da00d26662594f792
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-07-30 15:38:41 +0300

    dict as mapping

--- Objects/dictobject.c
+++ Objects/dictobject.c
@@ -150,7 +150,8 @@ dictinsert(dp, key, item)
 	}
 	where = keylookup(dct, key);
 	if (where >= 0) {
-		return replace(dct, where, item);
+		err_badcall();
+		return -1;
 	}
 	else {
 		return ins(dct, key, item);
@@ -257,7 +258,7 @@ object * //listobject*
 getdictkeys(dp)
 	object *dp;
 {
-		if (!is_dictobject(dp)) {
+	if (!is_dictobject(dp)) {
 		err_badcall();
 		return NULL;
 	}
@@ -319,6 +320,75 @@ dict_length(a)
 	return a->ob_size;
 }
 
+static object * //item
+dict_subscript(dp, str)
+	object *dp; //dictobject
+	object *str; //stringobject
+{
+	int i;
+	dictobject* dct;
+	char *key;
+	if (!is_dictobject(dp)) {
+		err_badcall();
+		return NULL;
+	}
+	else {
+		dct = (dictobject *) dp;
+	}
+	if (!is_stringobject(str)) {
+		err_badcall();
+		return NULL;
+	}
+	else {
+		key = ((stringobject *) str)->ob_sval;
+	}
+	i = keylookup(dct, key);
+	if (i >= 0) {
+		INCREF(dct->ob_item[i]);
+		return dct->ob_item[i];
+	}
+	else {
+		// err_setstr(KeyError, key);
+		return NULL;
+	}
+}
+
+static int //error
+dict_ass_subscript(dp, str, item)
+	object *dp; //dictobject
+	object *str; //string_object
+	object *item; //item
+{
+	dictobject* dct;
+	int where;
+	char *key;
+	if (is_listobject(dp))
+		dct = (dictobject *) dp;
+	else {
+		err_badarg();
+		return -1;
+	}
+	if (!is_stringobject(str)) {
+		err_badcall();
+		return -1;
+	}
+	else {
+		key = ((stringobject *) str)->ob_sval;
+	}
+	where = keylookup(dct, key);
+	if (where >= 0) {
+		return replace(dct, where, item);
+	}
+	else {
+		return ins(dct, key, item);
+	}
+}
+
+static mapping_methods dict_as_mapping = {
+	dict_length,		/*mp_length*/
+	dict_subscript,		/*mp_subscript*/
+	dict_ass_subscript,	/*mp_ass_subscript*/
+};
 
 typeobject Dicttype = {
 	OB_HEAD_INIT(&Typetype)
@@ -334,5 +404,5 @@ typeobject Dicttype = {
 	dict_repr,	/*tp_repr*/
 	NULL,		/*tp_as_number*/
 	NULL,//&dict_as_sequence,	/*tp_as_sequence*/
-	NULL,//&dict_as_mapping,		/*tp_as_mapping*/
+	&dict_as_mapping,		/*tp_as_mapping*/
 };
