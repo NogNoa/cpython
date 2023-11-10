d154bb84e94b89182acf84ea0dcb618efb0479cc
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-11-10 17:48:05 +0200

    protect compound statement macros

--- Include/exmalloc.h
+++ Include/exmalloc.h
@@ -1,10 +1,10 @@
 #include <malloc.h>
 
 #define NEW(type, size) ((type*) malloc(sizeof(type) * size))
-#define DEL(obj) (free(obj)); ((obj) = NULL)
+#define DEL(obj) {(free(obj)); ((obj) = NULL);}
 #define RESIZE(item, type, ob_size) (item = (type*) realloc(item, sizeof(type) * ob_size))
 
-#define XDEL(obj) if ((obj) == NULL) ; else DEL(obj)
+#define XDEL(obj) {if ((obj) == NULL) ; else DEL(obj);}
 
 #define ANY void
 
